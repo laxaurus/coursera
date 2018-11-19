b5c295ad59307dacd39d3d5d12e27c7099127b0d
astron <astron@astron>
2018-11-20 00:29:40 +0800

    ex5

---
+++ machine learning/machine-learning-ex5/ex5/myex5.m
@@ -0,0 +1,23 @@
+% =========== Part 2: Regularized Linear Regression Cost =============
+%  You should now implement the cost function for regularized linear 
+%  regression. 
+%
+
+theta = [1 ; 1];
+J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+
+fprintf(['Cost at theta = [1 ; 1]: %f '...
+         '\n(this value should be about 303.993192)\n'], J);
+
+fprintf('Program paused. Press enter to continue.\n');
+
+theta = [1 ; 1];
+[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+
+fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
+         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
+         grad(1), grad(2));
+
+fprintf('Program paused. Press enter to continue.\n');
+
+
